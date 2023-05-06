4c40e35c571ddc85f66cc203a42319ba426aca39
StevenQuintanaUMG <mquintanar2@miumg.edu.gt>
2023-05-06 10:57:33 -0600

    First

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using carro;
+
+Carro carro1 = new Carro();
+Carro carro2 = new Carro();
+
+carro1.Owner = "selvin";
+carro1.Marca = "tesla";
+carro1.Modelo = 2023;
+carro1.Color = "rojo";
+
+carro2.Owner = "nery";
+carro2.Marca = "ford";
+carro2.Modelo = 2022;
+carro2.Color = "black";
---
+++ clases/carro/Class1.cs
@@ -0,0 +1,59 @@
+﻿namespace carro;
+
+public class Carro
+{
+    public string Marca { get; set; }
+    public int Modelo { get; set; }
+    public string Color { get; set; }
+    public string Owner { get; set; }
+
+    private int Encendido = 1;
+    private int velocidad_actual = 0;
+
+    public int maxvel { get; set; }
+
+    public Carro(string nuevo_duenio, int _modelo, string _color, int _maxvel, string _marca)
+    {
+        this.Owner = nuevo_duenio;
+        this.Modelo = _modelo;
+        this.Color = _color;
+        this.Marca = _marca;
+
+        Console.WriteLine("Hola, tu carro esta creado y listo");
+    }
+
+    public void encender_carro()
+    {
+        if (Encendido == 0)
+        {
+            Console.WriteLine($"(this.Owner) tu carro lo encendido");
+            Encendido = 1;
+        }
+        else
+        {
+            Console.WriteLine($"{this.Owner} tu carro ya estaba encendido");
+        }
+    }
+
+    public void apagar_carro()
+    {
+        if (Encendido == 1)
+        {
+            Console.WriteLine($"{this.Owner} tu carro lo encendi");
+            Encendido = 0;
+        }
+        else
+        {
+            Console.WriteLine($"(this.Owner) tu carro ya estaba encendido");
+        }
+    }
+
+    public void acelerar(int meterCaite)
+    {
+        if (Encendido == 1)
+        {
+            velocidad_actual += meterCaite;
+            Console.WriteLine($"{Owner} vas a {velocidad_actual}");
+        }
+    }
+}
